aae6005c0583182a0367d3ff3ccd560c9069ed90
malekkhasawneh <91069222+malekkhasawneh@users.noreply.github.com>
2021-10-04 15:43:34 +0300

    Delete lib directory

--- lib/features/numbers/Data/datasource/random_number_local_data_source.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firstproject/features/numbers/Data/models/random_number_repository.dart';
-
-abstract class RandomNumberLocalDataSource {
-  Future<RandomNumberModel> getLastNumberTrivia();
-
-  Future<void> cacheNumberTrivia(RandomNumberModel triviaToCache);
-}
