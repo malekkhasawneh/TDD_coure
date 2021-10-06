343309e0d0fd1e228d26aacee1bf4458d0acb889
malekkhasawneh <91069222+malekkhasawneh@users.noreply.github.com>
2021-10-06 15:12:49 +0300

    Delete lib directory

--- lib/features/numbers/Data/datasource/random_number_remote_data_source.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firstproject/features/numbers/Data/models/random_number_model.dart';
-
-abstract class RandomNumberRemoteDataSource {
-  Future<RandomNumberModel> getConcreteRandomNumber(int number);
-
-  Future<RandomNumberModel> getRandomNumber();
-}
