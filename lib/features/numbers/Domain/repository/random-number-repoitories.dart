c1a94024236dbfde8bdabe6f6dda19edd290ee72
malekkhasawneh <91069222+malekkhasawneh@users.noreply.github.com>
2021-10-06 15:14:29 +0300

    Delete lib directory

--- lib/features/numbers/Domain/repository/random-number-repoitories.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:firstproject/core/error/errors.dart';
-import 'package:firstproject/features/numbers/Domain/entites/random_numbers.dart';
-
-abstract class RandomNumberRepository {
-  Future<Either<Errors, RandomNumber>> getConcreteRandomNumber(int number);
-
-  Future<Either<Errors, RandomNumber>> getRandomNumber();
-}
